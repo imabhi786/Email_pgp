79d0249035b7341d9ab40b67a52818e23c34f9a8
Abhijeet Mathur <abhijeetmathur786@gmail.com>
2020-04-25 23:27:46 +0530

    Added prikey for testing

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -25,10 +25,18 @@ router.post('/add_key', (req, res) => {
                     key_size: req.body.size,
                     password: req.body.password
                 });
+                bcrypt.genSalt(10, function (err, salt) {
+                    console.log('public key ->>>', newUser);
+                    bcrypt.hash(newUser.password, salt, function (err, hash) {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        
+                    });
+                });
                 const options = {
                     userIds: [{ name: req.body.name, email: req.body.email }],
                     numBits: parseInt(req.body.size),
-                    passphrase: req.body.password
+                    passphrase: newUser.password
                 };
                 var privkey, pubkey, revocationCertificate;
                 openpgp.generateKey(options).then(function (key) {
@@ -36,24 +44,17 @@ router.post('/add_key', (req, res) => {
                     pubkey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
                     revocationCertificate = key.revocationCertificate; // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
                     newUser.public_key = pubkey;
-                    newUser.private_key = pubkey;
+                    newUser.private_key = privkey;
                     global_key = key;
                     res.render("key-generate",{
                         key: global_key
                     })
                     console.log("key added successfully");
                 }).then(function () {
-                    bcrypt.genSalt(10, function (err, salt) {
-                        console.log('public key ->>>', newUser);
-                        bcrypt.hash(newUser.password, salt, function (err, hash) {
-                            if (err) throw err;
-                            newUser.password = hash;
-                            newUser
-                                .save()
-                                .then(user => console.log("User added to DB successfully"))
-                                .catch(err => console.log('main2', err));
-                        });
-                    });
+                    newUser
+                    .save()
+                    .then(user => console.log("User added to DB successfully"))
+                    .catch(err => console.log('main2', err));
                 })
                 .catch((err) => console.log('main', err));;
             }
@@ -114,7 +115,7 @@ router.post('/decrypt-message', (req, res) => {
     .findOne({email: req.body.email})
     .then( user => {
         const privkey = user.private_key;
-        const passphrase = `abc123`;
+        const passphrase = user.password;
         const encryptDecryptFunction = async () => {
             console.log("init1", openpgp)
             const privKeyObj = (await openpgp.key.readArmored(privkey)).keys[0]
