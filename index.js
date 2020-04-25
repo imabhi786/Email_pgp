e3b4ea3f93174c6ab24ddb9994f52748d957770f
leonatwork <noelabydas@gmail.com>
2020-04-25 10:15:05 +0530

    Created default layout

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 var logger = require('morgan');
 var dotenv = require('dotenv').config();
+const expressLayouts = require('express-ejs-layouts');
 
 const port = process.env.PORT || 3000;
 
 //Middleware for bodyparser
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
@@ -22,15 +23,16 @@ const db = require('./setup/myDBurl').mongoURL;
 
 //Attempt to connect to database
 mongoose.
-        connect(db,{ useNewUrlParser: true, useUnifiedTopology: true })
-        .then(()=> console.log('MongoDB connected successfully'))
-        .catch((err)=> console.log(err));
+    connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected successfully'))
+    .catch((err) => console.log(err));
 
 //Set view engine
-app.set('view engine' , 'ejs');
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 //Testing the server
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
@@ -39,9 +41,9 @@ const auth = require('./routes/api/auth');
 const home = require('./routes/api/home');
 
 
-app.use('/api/auth',auth);
-app.use('/api/home',home);
+app.use('/api/auth', auth);
+app.use('/api/home', home);
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running at ${port}`);
 });
